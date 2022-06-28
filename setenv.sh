f8678fb6fa658991eb24b1a4033e54fde9c7c4cc
J. Marc <marcjero@yahoo.com>
2022-06-28 19:02:54 +0200

    setenv

--- setenv.sh
+++ setenv.sh
@@ -2,4 +2,4 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 # export KCFLAGS="-fno-tree-loop-distribute-patterns -fno-store-merging"
-export KCFLAGS=
+export KCFLAGS=-mstrict-align
