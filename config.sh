4191cea2afc0df92660704abca9a863c3cea2d59
J. Marc <marcjero@yahoo.com>
2022-06-28 16:17:46 +0200

    configs

--- config.sh
+++ config.sh
@@ -5,8 +5,10 @@ CONFIG_DIR="`dirname $0`/configs"
 
 # ANDROID_CONFIG="$KERNEL_SRC/kernel/configs/android-base.config $KERNEL_SRC/kernel/configs/android-recommended.config"
 
-CONFIG_FILES="$KERNEL_SRC/arch/arm/configs/exynos_defconfig"
-CONFIG_FILES="$CONFIG_FILES $CONFIG_DIR//manta.config"
+#CONFIG_FILES="$KERNEL_SRC/arch/arm/configs/exynos_defconfig"
+CONFIG_FILES="$CONFIG_DIR//manta_defconfig"
+#CONFIG_FILES="$CONFIG_FILES $CONFIG_DIR/manta.config"
 CONFIG_FILES="$CONFIG_FILES $CONFIG_DIR/custom.config"
 
+rm .config
 "$KERNEL_SRC/scripts/kconfig/merge_config.sh" $CONFIG_FILES
